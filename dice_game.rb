a39bc1b1dfd817823f8633a5329c487f9d732773
ピーター <zsmash@FindTheTruth.local>
2013-02-06 16:02:11 -0800

    Roll some dice
    irb
    require 'dice_game.rb'
    Gameflow.reset_dice
    Gameflow.roll_dice
    Gameflow.print_dice_state

--- dice_game.rb
+++ dice_game.rb
@@ -1,19 +1,19 @@
-class Equipment
-  def initialize(dice_array)
-  end
-end
-
 class Dice
   def initialize(sides)
+    @sides = sides
+    @state = nil
   end
-  def roll
+  def roll_dice
+    @state = (rand*@sides).ceil
   end
-  def state
+  def get_state
+    @state
   end
 end
 
 class Player
-  def initialize(score)
+  def initialize
+    @score = 0
   end
 end
 
@@ -24,6 +24,7 @@ end
 
 class Gameflow
   @@players = []
+  @@dice = []
   @@turn = nil
   
   def Gameflow.current_turn
@@ -32,7 +33,7 @@ class Gameflow
   end
   #Adds a new player to the game
   def Gameflow.add_player
-    puts 'add a player'
+    @@players.push(Player.new)
   end
   #Sets the current turn
   def Gameflow.set_current_turn
@@ -42,8 +43,19 @@ class Gameflow
   def Gameflow.run_turn
     puts 'run the turn'
   end
+  def Gameflow.initialize_turn
+    @@turn = 0
+  end
+  def Gameflow.reset_dice
+    @@dice = nil
+    @@dice = []
+    1.upto(6) {|x| @@dice.push(Dice.new(6))}
+  end
+  def Gameflow.roll_dice
+    @@dice.each {|x| x.roll_dice}
+  end
+  def Gameflow.print_dice_state
+    @@dice.each {|x| puts x.get_state}
+  end
   private_class_method :new, :set_current_turn, :initialize_turn
 end
-
-
-Gameflow.add_player
