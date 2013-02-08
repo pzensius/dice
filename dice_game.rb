faa92d59fb6398978b327b9e0ad317b5452f5184
ピーター <zsmash@FindTheTruth.local>
2013-02-07 17:29:26 -0800

    Why did I ever write code without documentation??

--- dice_game.rb
+++ dice_game.rb
@@ -1,61 +1,117 @@
 class Dice
+  #Dice has number of sides
   def initialize(sides)
     @sides = sides
     @state = nil
   end
+  #Roll this dice
   def roll_dice
     @state = (rand*@sides).ceil
   end
+  #Get the state of the roll
   def get_state
     @state
   end
 end
 
 class Player
-  def initialize
-    @score = 0
+  #Player has a score and a name
+  def initialize(name)
+    @name = name
+    @score = Score.new
+  end
+  #Get this player's accumulated points
+  def get_accumulated_points
+    @score.get_points
+  end
+  #Get the player's name
+  def get_name
+    @name
+  end
+  #Reset the score for this player
+  def reset_score
+    @score = nil
+    @score = Score.new
   end
 end
 
 class Score
-  def initialize(points)
+  #Score has points
+  def initialize
+    @points = 0
+  end
+  #Add points
+  def add_points(points)
+    @points += points
+  end
+  #Get the current points
+  def get_points
+    @points
   end
 end
 
+#Main functions for the game
 class Gameflow
+  #The players in the game
   @@players = []
+  #The one and only dice
   @@dice = []
+  #Whose turn it is
   @@turn = nil
-  
-  def Gameflow.current_turn
-    @@turn = initialize_turn unless @@turn
+  #Game is running
+  @@running = nil
+  #Score for current turn
+  @@score = nil
+  #Get the current turn
+  def Gameflow.get_turn
     @@turn
   end
-  #Adds a new player to the game
-  def Gameflow.add_player
-    @@players.push(Player.new)
+  #Add a new player to the game
+  def Gameflow.add_player(name)
+    for x in @@players
+      if x.get_name == name
+        puts 'Name is already taken'
+        return false
+      end
+    end
+    @@players.push(Player.new(name))
   end
-  #Sets the current turn
-  def Gameflow.set_current_turn
-    puts 'set the current turn'
+  #List all the players and their scores
+  def Gameflow.list_players
+    for x in @@players
+      puts String(x.get_name) + ' ' + String(x.get_accumulated_points)
+    end
   end
-  #Runs a turn for a player
-  def Gameflow.run_turn
-    puts 'run the turn'
+  #Reset all scores
+  def Gameflow.reset_player_scores
+    @@players.each {|x| x.reset_score}
   end
-  def Gameflow.initialize_turn
-    @@turn = 0
+  #Set the current turn
+  def Gameflow.set_turn(at_player)
+    @@turn = at_player
   end
+  #Reset the score
+  def Gameflow.reset_score
+    @@score = nil
+    @@score = Score.new
+  end
+  #Reset all dice
   def Gameflow.reset_dice
     @@dice = nil
     @@dice = []
     1.upto(6) {|x| @@dice.push(Dice.new(6))}
   end
+  #Roll the dice
   def Gameflow.roll_dice
     @@dice.each {|x| x.roll_dice}
   end
+  #Print the current roll
   def Gameflow.print_dice_state
     @@dice.each {|x| puts x.get_state}
   end
-  private_class_method :new, :set_current_turn, :initialize_turn
+  #Check to see if already running
+  def Gameflow.is_running?
+    @@running
+  end
 end
+
